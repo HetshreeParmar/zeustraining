2822e5abb2b1fda8a83fdb4b1c46beb14a273aa0
HetshreeParmar <155625171+HetshreeParmar@users.noreply.github.com>
2024-01-16 18:42:53 +0530

    Update index.js

--- Task-5/index.js
+++ Task-5/index.js
@@ -1,89 +1,62 @@
 import cards from './data/data.js';
 
-
-const card_html = document.querySelector(".course__cards");
-
+const card_html = document.querySelector('.c-grid');
 for (let i = 0; i < 4; i++) {
     const card = document.createElement("div");
-    card.className = `cards card__${i+1}`;
-
-    const lessons = cards[i].unit ? ` <b>${cards[i].unit}</b> Units <b>${cards[i].lesson}</b> Lessons <b>${cards[i].nooftopic}</b> Topics ` : ` `;
-    const students = cards[i].number_of_students ? ` <p>${cards[i].number_of_students} Students</p> ` : ` `;
-    const dates = cards[i].date === "" ? ` ` : ` 
-        <div style="border: 1px solid rgba(0,0,0,0.12);"></div>
-        <p>${cards[i].date}</p>
-    `;
-
-    const dropDownStyle = cards[i].class.length ? "" : "color: rgba(0,0,0,0.4);";
+    card.className = `c-cards`;
+    const subjects = cards[i].subject ? `${cards[i].subject}<div class="pipe">|</div>
+        Grade
+        <div class="grade">${cards[i].grade}</div>
+        <div class="plus">${cards[i].kt}</div>` : ` `;
+    const lesseons = cards[i].unit ? ` <div class="number-line-2">${cards[i].unit}</div> Units 
+    <div class="number-line-2 lm">${cards[i].lesson}</div> Lessons 
+    <div class="number-line-2 lm">${cards[i].topic}</div> 
+    Topics ` : ` `;
 
     let classesOptions = ``;
-
     for (let j = 0; j < cards[i].class.length; j++) {
-        classesOptions += `<option value="Course_name">${cards[i].class[j]}</option>`
+        classesOptions += `<option value="${cards[i].class[j]}">${cards[i].class[j]}</option>`
         classesOptions += "\n";
     }
-
     classesOptions = classesOptions == "" ? `<option value="" disabled selected hidden style="color: rgba(0,0,0,0.4);">No Classes</option>` : classesOptions;
-
-    const icon2Opacity = cards[i].manage_course ? "1" : "0.4";
-    const icon3Opacity = cards[i].grade_submission ? "1" : "0.4";
-    const starFilter = cards[i].favorite ? "brightness(100%)" : "brightness(0) saturate(100%) invert(100%) sepia(2%) saturate(986%) hue-rotate(231deg) brightness(117%) contrast(87%);";
-    const expireTxt = cards[i].expiry ? `<div class="expired__txt">EXPIRED</div>` : "";
+    const students = cards[i].number_of_students ? ` <div>${cards[i].number_of_students} Students</div>` : ` `;
+    const dates = cards[i].date === "" ? ` ` :
+        `<div class="pipe">|</div><div>${cards[i].date}</div>`;
+    const previewClass = cards[i].preview ? "" : "i-disabled";
+    const manage_courseClass = cards[i].manage_course ? "" : "i-disabled";
+    const grade_submissionClass = cards[i].grade_submission ? "" : "i-disabled";
+    const reoprtClass = cards[i].report ? "" : "i-disabled";
+    // const expireTxt = cards[i].expiry ? `<div class="expired__txt">EXPIRED</div>` : "";
 
     card.innerHTML = `
-    ${expireTxt}
-    <div class="card__data">
-        
-        <img src=${cards[i].image} alt="">
-        <div class="card__texts">
-            <div class="card__heading">
-                <p>${cards[i].topic}</p>
-                <img style="filter:${starFilter}"" src="./Assets/icons/favourite.svg" alt="">
-            </div>
-            <br>
-            <div class="subject">
-                <p>${cards[i].subject}</p>
-                <div style="border: 1px solid rgba(0,0,0,0.12);"></div>
-                <p>${cards[i].grade} <span style="color: #1F7A54;">${cards[i].kt}</span></p>
-            </div>
-            <br>
-            <div class="topics">
-                ${lessons}
-            </div>
-            <br>
-            <form class="card__droupdown"action="">
-                <select name="course__name" style=${dropDownStyle} id="">
+        <div class="card">
+            <img src="${cards[i].image}" alt="" class="card-img">
+            <div class="card-details">
+                <div class="cardinfo1">
+                    <div class="card-title">${cards[i].title}</div>
+                    <img class="star" src="./icons/favourite.svg" alt="">
+                </div>
+                <div class="cardinfo2 regular-course-label">
+                    ${subjects}          
+                </div>
+                <div class="cardinfo3 regular-course-label">
+                    ${lesseons}     
+                </div>
+                <select name="select-class" class="card-select-input">
                     ${classesOptions}
                 </select>
-            </form>
-            <br>
-            <div class="subject">
-                ${students}
-                ${dates}
+                <div class="cardinfo4 regular-course-label">
+                    ${students}
+                    ${dates}
+                </div>
             </div>
         </div>
-    </div>
-    <br>
-    <hr>
-    <br>
-    <div class="card__icons">
-        <img src="./Assets/icons/preview.svg" alt="">
-        <img style="opacity:${icon2Opacity};" src="./Assets/icons/manage_course.svg" alt="">
-        <img style="opacity:${icon3Opacity};" src="./Assets/icons/grade_submissions.svg" alt="">
-        <img src="./Assets/icons/reports.svg" alt="">
-    </div>
-
+        <div class="card-icons">
+            <img class="${previewClass}" src="./icons/preview.svg" alt="">
+            <img class="${manage_courseClass}" src="./icons/manage course.svg" alt="">
+            <img class="${grade_submissionClass}" src="./icons/grade submissions.svg" alt="">
+            <img class="${reoprtClass}" src="./icons/reports.svg" alt="">
+        </div>
     `;
-
     card_html.appendChild(card);
 }
-
-// function openNav() {
-//     var x = document.querySelector(".header");
-//     if (x.className === "header") {
-//         x.className += " responsive";
-//         console.log(x.className);
-//     } else {
-//         x.className = "header";
-//     }
-// }
